be4c73b0e3b8be4e99d1b88cd41240624629c528
ANANDHABIB <162956323+ANANDHABIB@users.noreply.github.com>
2024-03-10 23:24:38 -0700

    Add files via upload

---
+++ exp1.py
@@ -0,0 +1,35 @@
+#python program to illustrate various operations on primitive datatype
+#operations on integer
+
+integer1=100
+integer2=10
+print("addition :",integer1+integer2)
+print("substration :",integer1-integer2)
+print("multiplication :",integer1*integer2)
+print("division :",integer1/integer2)
+
+print("\n")
+#operations on string
+string1="hello, "
+string2="good morning"
+print("addition of strings :",string1+string2)
+
+print("\n")
+#operations on float
+float1=5.4
+float2=2.4
+print("addition :",float1+float2)
+print("substration :",float1-float2)
+print("multiplication :",float1*float2)
+print("division :",float1/float2)
+
+print("\n")
+#operation on boolean
+#eligible is a boolean
+#False is assigned to eligible which is a boolean
+eligible=False
+age=15
+if (age>=18):
+    #True has been assigned to eligible which is boolean
+    eligible=True
+print(eligible)
